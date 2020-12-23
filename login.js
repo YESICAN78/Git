21ac2de68ffd7f6601bdc63c10c75972f06d7a1c
YESICAN78 <61768461+YESICAN78@users.noreply.github.com>
2020-12-23 17:04:28 +0800

    Update login.js

--- login.js
+++ login.js
@@ -16,13 +16,7 @@ function sum(a,b){
 return a+b;
 
 }
-
-<<<<<<< HEAD
-
-
 console.log("孙富林添加的一个函数")
-
-=======
 function add(){console.log("12346")}
 var newobj = {arr:[1,2,3,4,5]}
->>>>>>> c1d292a318ad8b4713886fe2e8f6611172c6d573
+
