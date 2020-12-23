6601d130b7f8d51830344f8871d692efa6823022
SunFuLin <2517097405@qq.com>
2020-12-23 21:11:49 +0800

    A用户添加的信息

--- login.js
+++ login.js
@@ -4,3 +4,13 @@ console.log(str)
 var user = "张三B"
 
 var user = "张三A"
+
+var obj = {
+	userInfo:{
+	 name:"A",
+	 age:45,
+	 height:178,
+	 like:["篮球","羽毛球","篮球"]
+	}
+
+}
--- regest.js
+++ regest.js
@@ -1,2 +1,12 @@
 var str = "我是注册变量"
 console.log(str)
+
+
+const userA = {
+     id:1,
+     name:"A",
+     height:178,
+     phone:18748628161,
+}
+
+
