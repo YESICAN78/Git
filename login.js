8bc2ad1ab4dcb833d97161e2b8a3bfa29ec4838c
SunFuLin <2517097405@qq.com>
2020-12-23 15:13:11 +0800

    "Add newobj"

--- login.js
+++ login.js
@@ -17,5 +17,10 @@ return a+b;
 
 }
 
+<<<<<<< HEAD
 
 console.log("孙富林添加的一个函数")
+=======
+function add(){console.log("12346")}
+var newobj = {arr:[1,2,3,4,5]}
+>>>>>>> c1d292a318ad8b4713886fe2e8f6611172c6d573
